ce9d6efab93c27b50a5b87b6bd3997cc5b22255a
Azzy 😝 <xiang.2@dublinstudents.net>
2025-09-07 16:40:41 +0000

    Try .js typing animation

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const header = document.querySelector('.animation h1');
+    const texts = [
+        "Chemical Engineer",
+        "Full-Stack Developer",
+        "Graphic Designer",
+        "Bird Advocate",
+    ];
+
+    let textIndex = 0;
+    let charIndex = 0;
+    let typing = true;
+
+    function type () {
+        if (typing) {
+            if (charIndex < texts[textIndex].length) {
+                header.textContent = texts[textIndex].slice(0,charIndex + 1);
+                charIndex++;
+                setTimeout(type,100);
+            } else {
+                typing = false;
+                setTimeout(type,1200);
+            }
+        } else {
+            if (charIndex > 0) {
+                header.textContent = texts[textIndex].slice(0,charIndex - 1);
+                charIndex--;
+                setTimeout(type,50);
+            } else {
+                typing = true;
+                textIndex = (textIndex + 1) % texts.length;
+                setTimeout(type,400);
+            }
+        }
+    }
+
+    type();
+
+});
 
 document.addEventListener('DOMContentLoaded', function() {
 const buttons = document.querySelectorAll('.nav-btn');
