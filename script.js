a6704cb3e6e3c673f1d16081dae242462ed31b82
Azzy 😝 <xiang.2@dublinstudents.net>
2025-09-07 16:11:09 +0000

    Comment out unfinished code parts

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 
-// document.addEventListener('DOMContentLoaded', function() {
-// const buttons = document.querySelectorAll('.nav-btn');
-// const sections = document.querySelectorAll('.content-section');
+document.addEventListener('DOMContentLoaded', function() {
+const buttons = document.querySelectorAll('.nav-btn');
+const sections = document.querySelectorAll('.content-section');
 
 
-// function activate(targetId) {
-// buttons.forEach(btn => btn.classList.toggle('active', btn.dataset.target === targetId));
-// sections.forEach(sec => sec.classList.toggle('active', sec.id === targetId));
+function activate(targetId) {
+buttons.forEach(btn => btn.classList.toggle('active', btn.dataset.target === targetId));
+sections.forEach(sec => sec.classList.toggle('active', sec.id === targetId));
 
-// history.replaceState(null, '', `#${targetId}`);
-// }
-
-
-// buttons.forEach(btn => {
-// btn.addEventListener('click', () => activate(btn.dataset.target));
-// });
+history.replaceState(null, '', `#${targetId}`);
+}
 
 
-// const hash = location.hash.replace('#','');
-// if (hash && document.getElementById(hash)) {
-// activate(hash);
-// } else {
-// activate('about');
-// }
-// });
+buttons.forEach(btn => {
+btn.addEventListener('click', () => activate(btn.dataset.target));
+});
+
+
+const hash = location.hash.replace('#','');
+if (hash && document.getElementById(hash)) {
+activate(hash);
+} else {
+activate('about');
+}
+});
